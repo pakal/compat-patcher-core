341b5495995c2fe6a611de08942f4515612135cd
Pascal Chambon <pythoniks@gmail.com>
2022-06-15 17:48:22 +0200

    Fix erroneous Trove classifiers (too bad, release 2.0 has them wrong)

--- setup.py
+++ setup.py
@@ -21,13 +21,11 @@ Intended Audience :: Developers
 Intended Audience :: Information Technology
 License :: OSI Approved :: MIT License
 Natural Language :: English
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.7
-Programming Language :: Python :: 3
-Programming Language :: Python :: 3.4
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
 Topic :: Software Development :: Libraries :: Python Modules
 Operating System :: Microsoft :: Windows
 Operating System :: Unix
